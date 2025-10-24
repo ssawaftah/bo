ac0262de7e789c00e9cd963133f9f9eea91cda9a
ssawaftah <74429413+ssawaftah@users.noreply.github.com>
2025-10-24 20:03:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# bot.py
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 import os
@@ -8,8 +7,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=["start"])
-async def start_command(message: types.Message):
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
     await message.reply(f"أهلاً وسهلاً بك، {message.from_user.full_name} 🌸")
 
 if __name__ == "__main__":
